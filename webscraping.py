c35322b452031fdebcfad4be9c9bbac2082a7986
Barış Atay <thebarisatay@gmail.com>
2025-09-02 18:27:50 +0300

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -4,10 +4,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
-
 def get_user_data(username1, username2):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    
+    options.binary_location = '/usr/bin/chromium'
+    
     browser = webdriver.Chrome(options=options)
     wait = WebDriverWait(browser, 10)
 
